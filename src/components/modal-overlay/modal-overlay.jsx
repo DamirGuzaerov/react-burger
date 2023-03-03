5591b129f5dccd0952d39855e502c4cbef31d24a
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-04 04:48:22 +0500

    [add] modal-overlay

---
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import modalOverlayStyles from "./modal-overlay.module.css"
+import {useModalClose} from "../../utils/hooks/useModalClose";
+import {useRef} from "react";
+
+const ModalOverlay = ({children, handleClose}) => {
+    const modalOverlayRef = useRef(null)
+
+    useModalClose(modalOverlayRef, handleClose)
+
+    return (
+        <div
+            ref={modalOverlayRef}
+            className={modalOverlayStyles.overlay}>
+            {children}
+        </div>
+    )
+}
+
+ModalOverlay.propTypes = {
+    children: PropTypes.node,
+    handleClose: PropTypes.func,
+}
+
+export default ModalOverlay
