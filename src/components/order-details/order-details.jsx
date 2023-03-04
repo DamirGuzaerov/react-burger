6ec54d1b53c85c96786dd27e1c2bde7989b8bc95
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-04 20:37:58 +0500

    [add] order-details

---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,19 @@
+import orderDetailsStyles from './order-details.module.css'
+import orderAcceptedImage from '../../images/orderAcceptedImage.png'
+
+const OrderDetails = () => {
+    return (
+        <div className={orderDetailsStyles.container}>
+            <h1 className={`${orderDetailsStyles['order-id']} text text_type_digits-large mb-8 pt-4`}>034536</h1>
+            <span className={'text text_type_main-default'}>Идентификатор заказа</span>
+            <img
+                className={'pt-15 mb-15'}
+                src={orderAcceptedImage}
+                alt="Order accepted"/>
+            <span className={'text text_type_main-small mb-2'}>Ваш заказ начали готовить</span>
+            <span className={'text  text_type_main-small text_color_inactive mb-15'}>Дождитесь готовности на орбитальной станции</span>
+        </div>
+    )
+}
+
+export default OrderDetails
