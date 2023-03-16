9841371a3a3882b019c0860458fb4e0e7f56c982
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-17 03:04:22 +0500

    [add] ingredients slice and thunk

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,20 +2,26 @@ import appStyles from './app.module.css';
 import {AppHeader} from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import useFetch from "../../utils/hooks/useFetch";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getBurgerIngredients} from "../../services/thunks/ingredients";
 
 function App() {
-    const {data, isLoading} = useFetch('api/ingredients')
+    const dispatch = useDispatch()
+    const {ingredients, ingredientsRequested} = useSelector(state => state.ingredients)
+    useEffect(()=>{
+        dispatch(getBurgerIngredients())
+    },[dispatch])
     return (
         <div className={appStyles['main-wrapper']}>
             <AppHeader/>
             <main className={appStyles.content}>
                 <div className={appStyles['burger-ingredients']}>
                     <h1 className={'text text_type_main-large pt-10 mb-5'}>Соберите бургер</h1>
-                    {isLoading ? 'Loading...' : <BurgerIngredients ingredients={data}/>}
+                    {ingredientsRequested ? 'Loading...' : <BurgerIngredients ingredients={ingredients}/>}
                 </div>
                 <div className={'pt-25'}>
-                    {isLoading ? 'Loading...' : <BurgerConstructor ingredients={data}/>}
+                    {ingredientsRequested ? 'Loading...' : <BurgerConstructor ingredients={ingredients}/>}
                 </div>
             </main>
         </div>
