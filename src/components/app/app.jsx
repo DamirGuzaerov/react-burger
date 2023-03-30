2eb22b75840e908845d331daf3ce5a4f27536faa
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-31 02:54:39 +0500

    [add] main layout

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,21 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MainPage} from "../../pages/main/main-page";
-
+import {LoginPage} from "../../pages/login/login-page";
+import {MainLayout} from "../layouts/main-layout/main-layout";
 
 function App() {
     return (
-       <BrowserRouter>
-           <Routes>
-               <Route path={'/'} element={<MainPage/>}/>
-           </Routes>
-       </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<MainLayout />}>
+                        <Route index element={<MainPage/>}/>
+                        <Route path={'/login'} element={<LoginPage/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+
     );
 }
 
