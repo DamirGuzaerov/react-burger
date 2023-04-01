825a31b2ef079058856967ddf033cfd3deb654bb
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-04-02 02:40:08 +0500

    [add] protected-route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,19 +7,29 @@ import {ForgotPasswordPage} from "../../pages/auth/forgot-password/forgot-passwo
 import {ResetPasswordPage} from "../../pages/auth/reset-password/reset-password-page";
 import {ProfileLayout} from "../layouts/profile-layout/profile-layout";
 import {ProfilePage} from "../../pages/profile/profile-page";
+import {OnlyAuth, OnlyUnAuth} from "../protected-route/protected-route";
+import {useEffect} from "react";
+import {getUser} from "../../services/thunks/user";
+import {useDispatch} from "react-redux";
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getUser())
+    },[dispatch])
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path={'/'} element={<MainLayout />}>
                         <Route index element={<MainPage/>}/>
-                        <Route path={'/login'} element={<LoginPage/>}/>
+                        <Route path={'/login'} element={<OnlyUnAuth element={<LoginPage/>}/>}/>
                         <Route path={'/registration'} element={<RegistrationPage/>}/>
                         <Route path={'/forgotPassword'} element={<ForgotPasswordPage/>}/>
                         <Route path={'/resetPassword'} element={<ResetPasswordPage/>}/>
-                        <Route path={'/profile'} element={<ProfileLayout/>}>
+                        <Route path={'/profile'} element={<OnlyAuth element={<ProfileLayout/>}/>}>
                             <Route index element={<ProfilePage/>}></Route>
                         </Route>
                     </Route>
