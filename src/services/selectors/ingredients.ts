492dd6a534e159d45c26f9ef2b7c4a7620cb348d
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-05-18 22:06:14 +0500

    [add] order ingredients selector

--- src/services/selectors/ingredients.ts
+++ src/services/selectors/ingredients.ts
@@ -2,3 +2,7 @@ import {RootState} from "../store";
 
 export const getIngredients = (state: RootState) => state.ingredients.ingredients
 export const getIngredientById = (state: RootState, id: string | undefined) => state.ingredients.ingredients.find(el => el._id === id)
+
+export const getIngredientsByIds = (state: RootState, ids: string[]) => state.ingredients.ingredients.filter(ingredient => {
+		return ids.includes(ingredient._id);
+});
